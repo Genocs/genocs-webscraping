91bf5d8e5a83862357be590522cc39d65a9bebf1
Nocco Giovanni Emanuele <giovanni.nocco@gmail.com>
2024-06-08 15:34:22 +0200

    chore: Update Flask and BeautifulSoup versions, add Flask-Restful, Flask-SQLAlchemy, and Flask-Swagger-UI dependencies

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import scraping_global_blue
 import scraping_planet
-from flask import Flask
+from flask import Flask, jsonify
 from gevent.pywsgi import WSGIServer
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+api = Api(app)
 
 
 @app.route('/')
@@ -27,13 +36,35 @@ def check_planet_document(docId=None):
     return result
 
 
+# Configure Swagger UI
+SWAGGER_URL = '/swagger1'
+API_URL = '/swagger1.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "TaxFree form tracker API"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+@app.route('/swagger1.json')
+def swagger():
+    with open('./swagger1.json', 'r') as f:
+        return jsonify(json.load(f))
+
+
 """
 The entry point
 Warning: Use 0.0.0.0 instead of 127.0.0.1 to avoid issue when use docker container   
 """
 if __name__ == '__main__':
     # Debug/Development
-    # app.run(debug=True, host="0.0.0.0", port="5400")
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host="0.0.0.0", port="5400")
     # Production
-    http_server = WSGIServer(('', 5400), app)
-    http_server.serve_forever()
+    # http_server = WSGIServer(('', 5400), app)
+    # http_server.serve_forever()
